1c583680ce680a42f5740f3e28ea797b2ba74546
wendell1101 <wendellchansuazo11@gmail.com>
2020-07-26 15:24:59 +0800

    Updates

---
+++ precache-manifest.ab4c239ca99e1478440fc47293c4cdeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe437cb6cf4cc477f320a82cd4f90b25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea48da1281aaf9732f9e",
+    "url": "/static/css/main.753fd437.chunk.css"
+  },
+  {
+    "revision": "e281b09eca7435c43fa5",
+    "url": "/static/js/2.63dc31b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.63dc31b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea48da1281aaf9732f9e",
+    "url": "/static/js/main.27b58e76.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
